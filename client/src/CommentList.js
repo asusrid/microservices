46cd1dd5a93fe5d69d09c58b698a5efd64c97a5b
asusrid <alexsurid@gmail.com>
2023-09-22 12:16:55 +0200

    query service included in client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
+const CommentList = ({ comments }) => {
 
-    const [comments, setComments] = useState([]);
-    
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-    
     const renderedComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>
     });
@@ -22,7 +10,7 @@ const CommentList = ({ postId }) => {
         <ul>
             {renderedComments}
         </ul>
-    )
+    );
 }
 
 export default CommentList;
